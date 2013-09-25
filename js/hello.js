e4ea93ea38840b6a8c7f384907f08379b328a3fb
Nathan <nathann@cs.uoregon.edu>
2013-09-24 21:47:36 -0700

    Ok, all done for now. Need to pick up my wife...
    Found a cool online less compiler.
    http://www.freeformatter.com/less-compiler.html

--- js/hello.js
+++ js/hello.js
@@ -1,4 +1,5 @@
 // Saying hello to backbone!
+// My first introduction to it...
 
 (function($){
 
@@ -7,9 +8,9 @@
     var Item = Backbone.Model.extend({
         // set some defaults...
         defaults: {
-            thing1: "Hello",
-            thing2: "world",
-            myBool: true
+            red: "#ff0000",
+            green: "#00ff00",
+            blue: "#0000ff"
         }
     });
 
@@ -30,7 +31,8 @@
             _.bindAll(this, 'render');
         },
         render: function(){
-            $(this.el).html('<span>'+this.model.get('thing1')+' '+this.model.get('thing2')+'</span>');
+        	
+            $(this.el).html('<span style="color:white;background:'+this.model.get('red')+';">'+this.model.get('red')+'</span>');
             return this;
         }
     });
@@ -38,7 +40,7 @@
     var ListView = Backbone.View.extend({
         // el, events, render, addItem, appendItem, oh my!
         // Whatchyou talkin' bout, Willis?
-        el: $('body'), // attaches to existing element
+        el: $('#c'), // attaches to existing element
         events: {
             'click button#add': 'addItem'
         },
@@ -50,12 +52,12 @@
             this.collection = new List();
             this.collection.bind('add', this.appendItem); // collection event binder
 
-            this.counter = 0;
+            this.counter = 100000;
             this.render(); // Ta da! A self rendering view, ladies and gentlemen
         },
         render: function(){
             var self = this;
-            $(this.el).append("<button id='add'>Add list item</button>");
+            $(this.el).append("<button id='add'>Add a little color, why don't ya?</button>");
             $(this.el).append("<ul></ul>");
             _(this.collection.models).each(function(item){ // in case collection is not empty
                 self.appendItem(item);
@@ -63,17 +65,21 @@
         },
 
         addItem: function(){
-            this.counter++;
-            var item = new Item();
-            item.set({
-                thing2: "universe " + this.counter
-            });
-            this.collection.add(item);
+        	var i;
+        	for(i=0; i <= 3; i++){
+	            this.counter += 111;
+    	        var item = new Item();
+        	    item.set({
+            	    red: "#" + this.counter
+      	      	});
+        	    this.collection.add(item);
+        	}
         },
         appendItem: function(item){
             var itemView = new ItemView({
                 model: item
             });
+            
             $('ul', this.el).append(itemView.render().el);
         }
     });
